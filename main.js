11b17a1b7bc38dd70e53ed72f31b61dae6c4a60e
radnerus <surendarandyou@gmail.com>
2020-01-01 13:35:39 +0530

    Error count and words completed added

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 'use strict';
 const randomWordEle = document.querySelector('#random-word');
 const typer = document.querySelector('#typer');
+const wordsCompletedEle = document.querySelector('#typed-words');
+const charactersMissSpelledEle = document.querySelector('#error-chars');
 
 let allWords = [];
 let currentRandomWords = [];
 let currentWord = '';
 let currentIndex = 0;
+let wordsCompleted = 0;
+let charactersMissSpelled = 0;
+let isInsert = null;
+let displayedWords = 4;
+
+let wordsToBeTested = 10;
+
+wordsCompletedEle.textContent = wordsCompleted;
+charactersMissSpelledEle.textContent = charactersMissSpelled;
 
 const getWords = async () => {
     const wordsResp = await fetch('words_array.json')
@@ -41,19 +52,32 @@ const getRandomWords = (length = 4) => {
 typer.addEventListener('input', (e) => {
     console.log(e.srcElement.value, currentWord);
     const typedWord = e.srcElement.value.toLowerCase();
+    isInsert = e.inputType === 'insertText';
     if (typedWord === currentWord) {
-        const newWord = getRandomWord();
-        currentWord = currentRandomWords[currentIndex + 1];
-        if (currentRandomWords.length < 7) {
-            currentRandomWords.push(newWord);
-            currentIndex = currentRandomWords.length - 4;
+        wordsCompleted++;
+        wordsCompletedEle.textContent = wordsCompleted;
+        if (displayedWords !== wordsCompleted) {
+            const newWord = getRandomWord();
+            currentWord = currentRandomWords[currentIndex + 1];
+            if (displayedWords < wordsToBeTested) {
+                if (currentRandomWords.length < 7) {
+                    currentRandomWords.push(newWord);
+                    currentIndex = currentRandomWords.length - 4;
+                } else {
+                    currentRandomWords = currentRandomWords.splice(1);
+                    currentRandomWords.push(newWord);
+                    currentIndex = 3;
+                }
+                displayedWords++;
+            } else {
+                currentIndex++;
+            }
+            randomWordEle.innerHTML = currentRandomWords.map(formatWords).join(' ');
+            highlightCurrentElement();
         } else {
-            currentRandomWords = currentRandomWords.splice(1);
-            currentRandomWords.push(newWord);
-            currentIndex = 3;
+            alert(`You have completed ${wordsCompleted} words with ${charactersMissSpelled} miss-spelled characters.`);
+            window.location.reload();
         }
-        randomWordEle.innerHTML = currentRandomWords.map(formatWords).join(' ');
-        highlightCurrentElement();
         typer.value = '';
     } else if (currentWord.indexOf(typedWord) !== 0) {
         highlightError();
@@ -63,7 +87,8 @@ typer.addEventListener('input', (e) => {
 })
 
 function removeError() {
-    document.querySelector('.error').classList.remove('error');
+    const errorEle = document.querySelector('.error');
+    errorEle && errorEle.classList.remove('error');
 }
 
 function highlightCurrentElement() {
@@ -80,6 +105,10 @@ function highlightCurrentElement() {
 
 function highlightError() {
     document.querySelector('.active').classList.add('error');
+    if (isInsert) {
+        charactersMissSpelled++;
+        charactersMissSpelledEle.textContent = charactersMissSpelled;
+    }
 }
 
 function formatWords(w, i) {

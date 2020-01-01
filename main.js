11a2be0655ac640d50ff882bcd308a954f0ee543
radnerus <surendarandyou@gmail.com>
2020-01-01 19:30:59 +0530

    Mobile design and timer added

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const randomWordEle = document.querySelector('#random-word');
 const typer = document.querySelector('#typer');
 const wordsCompletedEle = document.querySelector('#typed-words');
 const charactersMissSpelledEle = document.querySelector('#error-chars');
+const timeEle = document.querySelector('#time-taken');
+const notifications = document.querySelector('#notifications');
+const rules = document.querySelector('#rules');
+
+notifications.style.visibility = 'hidden';
 
 let allWords = [];
 let currentRandomWords = [];
@@ -12,11 +17,19 @@ let wordsCompleted = 0;
 let charactersMissSpelled = 0;
 let isInsert = null;
 let displayedWords = 4;
+let timeTaken = 0;
+let timeStarted = false;
 
 let wordsToBeTested = 10;
 
 wordsCompletedEle.textContent = wordsCompleted;
 charactersMissSpelledEle.textContent = charactersMissSpelled;
+timeEle.textContent = timeTaken;
+
+const showNotificationArea = () => {
+    notifications.style.visibility = 'visible';
+    rules.style.display = 'none';
+}
 
 const getWords = async () => {
     const wordsResp = await fetch('words_array.json')
@@ -25,9 +38,9 @@ const getWords = async () => {
 
     if (words.length) {
         document.querySelector('.overlay').classList.remove('overlay');
+        typer.focus();
     }
 
-    console.log('words', words.length);
     allWords = words;
     getRandomWords();
 }
@@ -46,45 +59,6 @@ const getRandomWords = (length = 4) => {
     highlightCurrentElement();
 }
 
-
-//Event Listeners
-
-typer.addEventListener('input', (e) => {
-    console.log(e.srcElement.value, currentWord);
-    const typedWord = e.srcElement.value.toLowerCase();
-    isInsert = e.inputType === 'insertText';
-    if (typedWord === currentWord) {
-        wordsCompleted++;
-        wordsCompletedEle.textContent = wordsCompleted;
-        if (displayedWords !== wordsCompleted) {
-            const newWord = getRandomWord();
-            currentWord = currentRandomWords[currentIndex + 1];
-            if (displayedWords < wordsToBeTested) {
-                if (currentRandomWords.length < 7) {
-                    currentRandomWords.push(newWord);
-                    currentIndex = currentRandomWords.length - 4;
-                } else {
-                    currentRandomWords = currentRandomWords.splice(1);
-                    currentRandomWords.push(newWord);
-                    currentIndex = 3;
-                }
-                displayedWords++;
-            } else {
-                currentIndex++;
-            }
-            randomWordEle.innerHTML = currentRandomWords.map(formatWords).join(' ');
-            highlightCurrentElement();
-        } else {
-            alert(`You have completed ${wordsCompleted} words with ${charactersMissSpelled} miss-spelled characters.`);
-            window.location.reload();
-        }
-        typer.value = '';
-    } else if (currentWord.indexOf(typedWord) !== 0) {
-        highlightError();
-    } else {
-        removeError();
-    }
-})
 
 function removeError() {
     const errorEle = document.querySelector('.error');
@@ -114,4 +88,59 @@ function highlightError() {
 function formatWords(w, i) {
     return `<span id="word-${i}" class="words">${w}</span>`;
 }
+
+const startTimer = () => {
+    timeStarted = true;
+    showNotificationArea();
+    setInterval(() => {
+        timeTaken++;
+        timeEle.textContent = timeTaken;
+    }, 1000);
+}
+
+
+//Event Listeners
+
+typer.addEventListener('input', (e) => {
+    console.log(e.srcElement.value, currentWord);
+    const typedWord = e.srcElement.value.toLowerCase();
+    isInsert = e.inputType === 'insertText';
+    if (isInsert && !timeStarted) {
+        startTimer();
+    }
+    if (typedWord === currentWord) {
+        wordsCompleted++;
+        wordsCompletedEle.textContent = wordsCompleted;
+        if (displayedWords !== wordsCompleted) {
+            const newWord = getRandomWord();
+            currentWord = currentRandomWords[currentIndex + 1];
+            if (displayedWords < wordsToBeTested) {
+                if (currentRandomWords.length < 7) {
+                    currentRandomWords.push(newWord);
+                    currentIndex = currentRandomWords.length - 4;
+                } else {
+                    currentRandomWords = currentRandomWords.splice(1);
+                    currentRandomWords.push(newWord);
+                    currentIndex = 3;
+                }
+                displayedWords++;
+            } else {
+                currentIndex++;
+            }
+            randomWordEle.innerHTML = currentRandomWords.map(formatWords).join(' ');
+            highlightCurrentElement();
+        } else {
+            setTimeout(() => {
+                alert(`You have completed ${wordsCompleted} words with ${charactersMissSpelled} miss-spelled characters in ${timeTaken} seconds.`);
+                window.location.reload();
+            }, 10);
+        }
+        typer.value = '';
+    } else if (currentWord.indexOf(typedWord) !== 0) {
+        highlightError();
+    } else {
+        removeError();
+    }
+})
+
 
